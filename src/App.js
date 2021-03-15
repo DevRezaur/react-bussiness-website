c050bea74c6bba6108b8f6937710d12c94bb5b8c
Rezaur Rahman <rezaur.official@gmail.com>
2021-03-15 23:08:19 +0600

    Added: MiniNav component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import GlobalStyle from './globalStyles';
-import { Footer, Navbar } from './components';
+import { Footer } from './components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/HomePage/Home';
+import Signin from './pages/SigninPage/Signin';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <GlobalStyle />
-      <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/sign-in' exact component={Signin} />
       </Switch>
       <Footer />
     </Router>
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { aboutObject, mentorshipObject, testimonialObject } from './Data'; 
-import { CourseSection, InfoSection } from '../../components'
+import { CourseSection, InfoSection, Navbar } from '../../components';
 
 const Home = () => {
     return (
         <>
+            <Navbar />
             <InfoSection {...aboutObject} />
             <InfoSection {...mentorshipObject} />
             <InfoSection {...testimonialObject} />
